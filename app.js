520d3cfaf6ad382fce44367195eb8d62a7b06472
GabilaB <willibroad@naver.com>
2018-11-19 20:23:04 +0900

    app file change

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.get("/blogs/new", function(req, res) {
 //CREATE ROUTE
 app.post("/blogs", function(req, res) {
   //create blog
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.create(req.body.blog, function(err, newBlog) {
     if (err) {
       res.render("new");
@@ -88,6 +89,7 @@ app.get("/blogs/:id/edit", function(req, res) {
 // UPDATE ROUTE
 
 app.put("/blogs/:id", function(req, res) {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(
     err,
     foundBlog
